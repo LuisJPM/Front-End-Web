4a2ac79bed4ae6336c7f329b308eae6a6e410fb5
Informatica <informatica@alumno.local>
2025-04-08 07:16:43 -0600

    Creacion de Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import {useRoutes} from "react-router-dom"
-import {Toaster} from "react-hot-toast"
-import routes from "./routes.jsx"
+import { useRoutes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import routes from "./routes.jsx";
 
 
 export const App = () => {
 
-let element = useRoutes(routes);
+  let element = useRoutes(routes);
 
-  return(
+  return (
     <>
       {element}
       <Toaster
-        position="bottom-rigth"
+        position="bottom-right"
         reverseOrder={false}
       />
     </>
